bbb7606ad3117853ff74f2469d7ed2bc1e5dbea9
Danny Staple <danny@orionrobots.co.uk>
2021-03-01 23:34:45 +0000

    Add web control code for youtube tutorial

---
+++ demos/web_arm/arm_server.py
@@ -0,0 +1,35 @@
+import usb_arm
+from flask import Flask, Response, request
+
+
+arm = usb_arm.Arm()
+app = Flask(__name__)
+
+movemap = {
+    "GripsClose": usb_arm.GripsClose,
+    "CloseGrips": usb_arm.CloseGrips,
+    "GripsOpen": usb_arm.GripsOpen,
+    "OpenGrips": usb_arm.OpenGrips,
+    "Stop": usb_arm.Stop,
+    "WristUp": usb_arm.WristUp,
+    "WristDown": usb_arm.WristDown,
+    "ElbowUp": usb_arm.ElbowUp,
+    "ElbowDown": usb_arm.ElbowDown,
+    "ShoulderUp": usb_arm.ShoulderUp,
+    "ShoulderDown": usb_arm.ShoulderDown,
+    "BaseClockWise": usb_arm.BaseClockWise,
+    "BaseCtrClockWise": usb_arm.BaseCtrClockWise,
+    "LedOn": usb_arm.LedOn
+}
+
+@app.route('/move', methods=['POST'])
+def move():
+    pattern = movemap[request.form['pattern']]
+
+    def inner():
+            yield "starting<br>\n"
+            arm.move(pattern)
+            yield "completed\n"
+    return Response(inner())
+
+app.run(host="0.0.0.0")
