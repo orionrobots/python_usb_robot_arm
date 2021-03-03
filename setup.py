90f50d9be109c0d11cb72361eaa6ca9b0a448633
Danny Staple <danny@orionrobots.co.uk>
2021-03-03 09:08:48 +0000

    Oops - made the readme markdown without telling setup.py
    Fixes issue #3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['usb_arm',],
     author="Danny Staple - Orionrobots",
     license='Creative Commons By Attribution Share-Alike v3.0',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         "pyusb"
     ]
