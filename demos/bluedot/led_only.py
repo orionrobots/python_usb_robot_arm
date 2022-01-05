eda8b327cf665f049dce03ecb91321c2df072f4a
Danny Staple <danny@orionrobots.co.uk>
2022-01-05 16:24:40 +0000

    Clear pep8 issues

--- demos/bluedot/led_only.py
+++ demos/bluedot/led_only.py
@@ -4,11 +4,14 @@ import usb_arm
 
 arm = usb_arm.Arm()
 
+
 def led_pressed(pos):
     arm.tell(usb_arm.LedOn)
 
+
 def stop(*args):
     arm.tell(usb_arm.Stop)
+
 
 bd = BlueDot(cols=3, rows=2)
 led = bd[1, 0]
