a9ee904be3ae9c706e125f8bab4cbe1f2a15982f
Danny Staple <danny@orionrobots.co.uk>
2022-01-05 16:28:12 +0000

    Pep8

--- demos/web_arm/arm_hello.py
+++ demos/web_arm/arm_hello.py
@@ -5,12 +5,14 @@ from flask import Flask, Response
 arm = usb_arm.Arm()
 app = Flask(__name__)
 
+
 @app.route('/')
 def flash():
-  def inner():
-      yield "starting</br>\n"
-      arm.move(usb_arm.LedOn)
-      yield "completed\n"
-  return Response(inner())
+    def inner():
+        yield "starting</br>\n"
+        arm.move(usb_arm.LedOn)
+        yield "completed\n"
+    return Response(inner())
+
 
 app.run(host="0.0.0.0")
