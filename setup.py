e07b9a095b5399ce1ab1c5563e1121779daaae91
Danny Staple <danny@orionrobots.co.uk>
2025-01-08 12:39:42 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='usb_arm',
-    version='0.1.1',
-    packages=['usb_arm',],
+    name='usb_robot_arm',
+    version='0.2.0',
+    packages=['usb_robot_arm',],
     author="Danny Staple - Orionrobots",
     license='Creative Commons By Attribution Share-Alike v3.0',
     long_description=open('README.md').read(),
