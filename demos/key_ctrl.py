7dcd51accd48f10a885ae04cc44321e2522a8380
Danny Staple <danny@orionrobots.co.uk>
2025-01-26 12:16:32 +0000

    Park demos for more fixing

--- demos/key_ctrl.py
+++ demos/key_ctrl.py
@@ -1,16 +1,21 @@
 """key_ctrl - key based arm controller"""
 from functools import partial
-import owi_maplin_robot_arm
+import logging
+import sys
+
 import pygame
 from pygame.locals import *
-import sys
+
+import owi_maplin_usb_arm as usb_arm
 
 
 def handle_key(arm, delay, key_map, key):
     def do_it():
         if key in key_map:
-            print("Doing key ", key)
-            arm.move(key_map[key], delay)
+            message = key_map[key]
+            print("Key ", key, "Movement message", message)
+
+            arm.move(message, delay)
     arm.safe_tell(do_it)
 
 
@@ -18,22 +23,23 @@ def make_keymap():
     """Bp - an initialised arm bitpattern.
     returns the keymap"""
     return {
-        K_z: owi_maplin_robot_arm.BaseClockWise,
-        K_x: owi_maplin_robot_arm.BaseCtrClockWise,
-        K_r: owi_maplin_robot_arm.CloseGrips,
-        K_f: owi_maplin_robot_arm.OpenGrips,
-        K_a: owi_maplin_robot_arm.ShoulderDown,
-        K_q: owi_maplin_robot_arm.ShoulderUp,
-        K_s: owi_maplin_robot_arm.ElbowDown,
-        K_w: owi_maplin_robot_arm.ElbowUp,
-        K_d: owi_maplin_robot_arm.WristDown,
-        K_e: owi_maplin_robot_arm.WristUp}
+        K_z: usb_arm.BaseClockWise,
+        K_x: usb_arm.BaseCtrClockWise,
+        K_r: usb_arm.CloseGrips,
+        K_f: usb_arm.OpenGrips,
+        K_a: usb_arm.ShoulderDown,
+        K_q: usb_arm.ShoulderUp,
+        K_s: usb_arm.ElbowDown,
+        K_w: usb_arm.ElbowUp,
+        K_d: usb_arm.WristDown,
+        K_e: usb_arm.WristUp,
+        K_l: usb_arm.LedOn}
 
 
 def key_loop():
     km = make_keymap()
     try:
-        arm = owi_maplin_robot_arm.Arm()
+        arm = usb_arm.Arm()
     except AttributeError:
         print("Please make sure the arm is connected and turned on")
         sys.exit(1)
@@ -53,7 +59,11 @@ def key_loop():
 
 
 def main():
+    logging.basicConfig()
+    usb_arm.logger.setLevel(logging.DEBUG)
     pygame.init()
+    pygame.display.set_mode([200, 200])
+    print("Press z/x to turn the base motor")
     key_loop()
 
 
---
+++ demos/simple.py
@@ -0,0 +1,13 @@
+import usb_robot_arm as usb_arm
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+arm = usb_arm.Arm()
+
+arm.move(usb_arm.LedOn)
+print("Wrist up")
+arm.move(usb_arm.WristUp)
+print("Wrist down")
+arm.move(usb_arm.WristDown)
+
